a6b3311d2894f7cf5b46118bf2286029235bba21
ashwini-sundarrajan <ashwinisundarr@gmail.com>
2025-10-03 19:30:56 +0530

    Added

--- script.js
+++ script.js
@@ -172,3 +172,17 @@ function addToPlaylist() {
   document.getElementById("playlist").appendChild(playlistContainer);
   input.value = "";
 }
+
+const fileInput = document.getElementById('fileInput');
+    const profileImage = document.getElementById('profileImage');
+
+    fileInput.addEventListener('change', function() {
+      const file = this.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+          profileImage.src = e.target.result; // change the circle image
+        }
+        reader.readAsDataURL(file);
+      }
+    });
